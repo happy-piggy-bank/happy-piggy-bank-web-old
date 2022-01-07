64a030e7524b83975080015327ee90ed0c871e10
Gary Lee <dokdo2005@gmail.com>
2022-01-07 22:47:55 +0900

    index에 react router 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux'
 import store from './features/store'
 
@@ -10,7 +10,9 @@ import './css/index.css';
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
     </React.StrictMode>
   </Provider>,
   document.getElementById('root')
