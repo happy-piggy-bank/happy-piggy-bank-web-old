b2b34ee209ee68c76799c033d2804a92108f107d
Gary Lee <dokdo2005@gmail.com>
2022-01-07 22:15:10 +0900

    전체적인 CSS 수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './css/app.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="mainContainer">
       <p>아직 준비중이에요!</p>
     </div>
   );
