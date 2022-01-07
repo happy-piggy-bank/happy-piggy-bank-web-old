8f32adf2ce9992052ff5f684dd895cf0fc6704e7
Gary Lee <dokdo2005@gmail.com>
2022-01-08 01:10:57 +0900

    메인 페이지 컴포넌트 디자인 완료

--- src/components/mainScreen.jsx
+++ src/components/mainScreen.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import piggyBankImg from '../images/piggy_bank.png';
 import '../css/mainScreen.css';
 import '../css/common.css';
 
@@ -10,10 +11,18 @@ const MainScreen = () => {
                 <div className='descriptionText'>여러분의 행복했던 순간을 저금해보세요</div>
             </div>
             <div className='bodyArea'>
-                <button className='primaryButton'>시작하기</button>
+                <img src={piggyBankImg} width="60%" alt='' />
+                <p>&nbsp;</p>
+                <p>0명의 사람들이</p>
+                <p>0개의 행복했던 순간과 함께</p>
+                <p>0원을 저금하고 있어요</p>
+                <p>&nbsp;</p>
+                <button className='primaryButton' onClick={() => alert('아직 오픈 전이에요!')}>시작하기</button>
             </div>
             <div className='footerArea'>
-                <div className='footerText'>Made by 김뷰엘 with ♥</div>
+                <div className='footerText'>
+                    <p>made by 김뷰엘 with ♥</p>
+                </div>
             </div>
         </div>
     );
