c52fe1131fb2be702264a05edaf0f259fb4476f5
Gary Lee <dokdo2005@gmail.com>
2022-01-08 00:12:38 +0900

    메인 화면 추가

--- src/components/mainScreen.jsx
+++ src/components/mainScreen.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import '../css/mainScreen.css';
+import '../css/common.css';
 
 const MainScreen = () => {
     return (
         <div className='mainScreenContainer'>
-            <p>아직 준비중이에요!</p>
+            <div className='headerArea'>
+                <div className='titleText'>나의 행복한 돼지 저금통</div>
+                <div className='descriptionText'>여러분의 행복했던 순간을 저금해보세요</div>
+            </div>
+            <div className='bodyArea'>
+                <button className='primaryButton'>시작하기</button>
+            </div>
+            <div className='footerArea'>
+                <div className='footerText'>Made by 김뷰엘 with ♥</div>
+            </div>
         </div>
     );
 }
