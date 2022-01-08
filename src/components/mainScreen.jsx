33e6546957fde2c54771f13794bcf8bfffac9399
Gary Lee <dokdo2005@gmail.com>
2022-01-08 18:22:30 +0900

    메인 화면 CSS 수정

--- src/components/mainScreen.jsx
+++ src/components/mainScreen.jsx
@@ -1,13 +1,12 @@
 import { React, useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 
-import piggyBankImg from '../images/piggy_bank.png';
-import '../css/common/mainHeader.css';
-import '../css/common/mainFooter.css';
-
 import MainHeader from './common/mainHeader';
 import MainFooter from './common/mainFooter';
 import PrimaryButton from './common/primaryButton';
+
+import piggyBankImg from '../images/piggy_bank.png';
+import '../css/mainScreen.css';
 
 const MainScreen = () => {
     const [mainScreenRoute, setMainScreenRoute] = useState(null);
@@ -26,12 +25,14 @@ const MainScreen = () => {
         <div className='mainScreenContainer'>
             <MainHeader/>
             <div className='mainBodyArea'>
-                <img src={piggyBankImg} width="60%" alt='돼지 저금통' />
-                <p>&nbsp;</p>
-                <p>0명의 사람들이</p>
-                <p>0개의 행복했던 순간과 함께</p>
-                <p>0원을 저금하고 있어요</p>
-                <p>&nbsp;</p>
+                <div className='mainContentArea'>
+                    <img src={piggyBankImg} width="60%" alt='돼지 저금통' />
+                    <p>&nbsp;</p>
+                    <p>0명의 사람들이</p>
+                    <p>0개의 행복했던 순간과 함께</p>
+                    <p>0원을 저금하고 있어요</p>
+                    <p>&nbsp;</p>
+                </div>
                 <PrimaryButton buttonText="시작하기" onClick={() => navigate(mainScreenRoute)} />
             </div>
             <MainFooter/>
