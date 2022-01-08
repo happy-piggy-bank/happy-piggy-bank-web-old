f67eed2dbe1f89662d442277987b5378c18660e2
Gary Lee <dokdo2005@gmail.com>
2022-01-08 18:32:51 +0900

    로그인 화면 css 추가

--- src/components/members/loginScreen.jsx
+++ src/components/members/loginScreen.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
+import "../../css/members/loginScreen.css";
+
 import MainHeader from '../common/mainHeader';
 import MainFooter from '../common/mainFooter';
+import PrimaryButton from '../common/primaryButton'; 
 
 const LoginScreen = () => {
     return (
         <div className='loginScreenContainer'>
             <MainHeader/>
             <div className='loginBodyArea'>
-                <p>로그인 화면</p>
+                <div className='loginBoxArea'>
+                    <p>로그인 화면</p>
+                </div>
+                <PrimaryButton buttonText={"저금 계속하기"} onClick={() => {}}/>
+                <PrimaryButton buttonText={"저금 새로 시작하기"} onClick={() => {}}/>
             </div>
             <MainFooter/>
         </div>
