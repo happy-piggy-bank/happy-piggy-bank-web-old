18cb2eee1c1294d8f819afb4edcf414b1326db92
Gary Lee <dokdo2005@gmail.com>
2022-01-09 05:22:01 +0900

    공용 Input Bar 로직 수정

--- src/components/common/inputBar.jsx
+++ src/components/common/inputBar.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import { React, useRef } from 'react';
 import '../../css/common/inputBar.css';
 
-const InputBar = ({ placeHolder, isPassword }) => {
+const InputBar = ({ placeHolder, isPassword, onChange }) => {
+    const textRef = useRef();
     return (
         <div className="inputBarContainer">
             <input
                 type={isPassword ? 'password' : 'text'}
                 className='inputBarInputArea'
-                placeholder={placeHolder} />
+                placeholder={placeHolder}
+                ref={textRef}
+                onChange={() => onChange(textRef.current.value)} />
         </div>
     )
 }
