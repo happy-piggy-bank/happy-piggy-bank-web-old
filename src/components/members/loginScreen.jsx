7a94d2cb3d89a8823027ec7666937d5aa7233639
Gary Lee <dokdo2005@gmail.com>
2022-01-09 00:25:21 +0900

    로그인 화면 디자인 추가

--- src/components/members/loginScreen.jsx
+++ src/components/members/loginScreen.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 import "../../css/members/loginScreen.css";
 
 import MainHeader from '../common/mainHeader';
 import MainFooter from '../common/mainFooter';
-import PrimaryButton from '../common/primaryButton'; 
+import PrimaryButton from '../common/primaryButton';
+
+import InputBar from '../common/inputBar';
 
 const LoginScreen = () => {
+    const navigate = useNavigate();
+
     return (
         <div className='loginScreenContainer'>
             <MainHeader/>
             <div className='loginBodyArea'>
                 <div className='loginBoxArea'>
-                    <p>로그인 화면</p>
+                    <InputBar placeHolder={"아이디 (이메일 주소)"} />
+                    <InputBar placeHolder={"비밀번호"} isPassword={true} />
                 </div>
                 <PrimaryButton buttonText={"저금 계속하기"} onClick={() => {}}/>
-                <PrimaryButton buttonText={"저금 새로 시작하기"} onClick={() => {}}/>
+                <PrimaryButton buttonText={"저금 새로 시작하기"} onClick={() => navigate('/join')}/>
             </div>
             <MainFooter/>
         </div>
