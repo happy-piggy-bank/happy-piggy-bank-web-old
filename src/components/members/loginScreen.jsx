8eef367acec1e8df4a2231abb197e5504cd4c3d3
Gary Lee <dokdo2005@gmail.com>
2022-01-09 02:08:40 +0900

    로그인 화면 디자인 수정

--- src/components/members/loginScreen.jsx
+++ src/components/members/loginScreen.jsx
@@ -5,8 +5,8 @@ import "../../css/members/loginScreen.css";
 
 import MainHeader from '../common/mainHeader';
 import MainFooter from '../common/mainFooter';
-import PrimaryButton from '../common/primaryButton';
 
+import PrimaryButton from '../common/primaryButton';
 import InputBar from '../common/inputBar';
 
 const LoginScreen = () => {
@@ -20,8 +20,11 @@ const LoginScreen = () => {
                     <InputBar placeHolder={"아이디 (이메일 주소)"} />
                     <InputBar placeHolder={"비밀번호"} isPassword={true} />
                 </div>
-                <PrimaryButton buttonText={"계속하기"} onClick={() => {}}/>
-                <PrimaryButton buttonText={"새로 시작하기"} onClick={() => navigate('/join')}/>
+                <PrimaryButton buttonText={"로그인"} onClick={() => {}}/>
+                <div className="joinLinkText">
+                    처음 오셨나요?&nbsp;
+                    <a href="" onClick={() => navigate('/join')}>회원가입</a>
+                </div>
             </div>
             <MainFooter/>
         </div>
