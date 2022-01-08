b783eab75cbd3b9f1cba4a31c1846be3a8cb3a39
Gary Lee <dokdo2005@gmail.com>
2022-01-09 02:19:56 +0900

    회원가입 화면 디자인 추가

--- src/components/members/joinScreen.jsx
+++ src/components/members/joinScreen.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
+
+import '../../css/members/joinScreen.css';
 
 import MainHeader from '../common/mainHeader';
 import MainFooter from '../common/mainFooter';
 
+import PrimaryButton from '../common/primaryButton';
+import InputBar from '../common/inputBar';
+
 const JoinScreen = () => {
+    const navigate = useNavigate();
+
     return (
         <div className='joinScreenContainer'>
             <MainHeader/>
             <div className='joinBodyArea'>
-                <p>회원가입 화면</p>
+                <div className='joinBoxArea'>
+                    <InputBar placeHolder={"아이디 (이메일 주소)"} />
+                    <InputBar placeHolder={"비밀번호"} isPassword={true} />
+                    <InputBar placeHolder={"이름"} />
+                </div>
+                <PrimaryButton buttonText={"회원가입"} onClick={() => {}}/>
+                <div className="loginLinkText">
+                    이미 계정이 있으신가요?&nbsp;
+                    <a href="" onClick={() => navigate('/login')}>로그인</a>
+                </div>
             </div>
             <MainFooter/>
         </div>
