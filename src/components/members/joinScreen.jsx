50ec2a6341a577c59209ca41803945838ff59be8
Gary Lee <dokdo2005@gmail.com>
2022-01-09 05:38:47 +0900

    회원가입 화면에 state 추가

--- src/components/members/joinScreen.jsx
+++ src/components/members/joinScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
 import '../../css/members/joinScreen.css';
@@ -10,6 +10,9 @@ import PrimaryButton from '../common/primaryButton';
 import InputBar from '../common/inputBar';
 
 const JoinScreen = () => {
+    const [userEmail, setEmail] = useState(null);
+    const [userPw, setPw] = useState(null);
+    const [userName, setName] = useState(null);
     const navigate = useNavigate();
 
     return (
@@ -17,11 +20,11 @@ const JoinScreen = () => {
             <MainHeader/>
             <div className='joinBodyArea'>
                 <div className='joinBoxArea'>
-                    <InputBar placeHolder={"아이디 (이메일 주소)"} />
-                    <InputBar placeHolder={"비밀번호"} isPassword={true} />
-                    <InputBar placeHolder={"이름"} />
+                    <InputBar placeHolder={"아이디 (이메일 주소)"} onChange={setEmail} />
+                    <InputBar placeHolder={"비밀번호"} isPassword={true} onChange={setPw} />
+                    <InputBar placeHolder={"이름"} onChange={setName} />
                 </div>
-                <PrimaryButton buttonText={"회원가입"} onClick={() => {}}/>
+                <PrimaryButton buttonText={"회원가입"} onClick={() => console.log({ userEmail, userPw, userName })}/>
                 <div className="loginLinkText">
                     이미 계정이 있으신가요?&nbsp;
                     <a href="" onClick={() => navigate('/login')}>로그인</a>
