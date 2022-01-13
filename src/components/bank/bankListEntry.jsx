a581d183822f1c95a8fd678cca0b01485def9196
Gary Lee <dokdo2005@gmail.com>
2022-01-13 21:36:36 +0900

    삭제 버튼 추가

--- src/components/bank/bankListEntry.jsx
+++ src/components/bank/bankListEntry.jsx
@@ -1,7 +1,7 @@
 import { React } from 'react';
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoins, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import { faCoins, faCalendarAlt, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 import '../../css/bank/bankListEntry.css';
 
@@ -12,6 +12,9 @@ const BankListEntry = () => {
     return (
         <div className='bankListEntryContainer'>
             <img className='bankListEntryHeadImg' src={entryHeaderImg} />
+            <button className='bankListEntryDeleteButton'>
+                <FontAwesomeIcon icon={faTimes} />
+            </button>
             <div className='bankListEntryContentArea'>
                     <img src={noImage} width='200px' />
                     <div className='bankListEntryTextArea'>
