8850b4ad0ed9942ceaa39678ac2e8390986967c3
Gary Lee <dokdo2005@gmail.com>
2022-01-16 08:47:46 +0900

    회원정보 화면 디자인 추가

--- src/components/members/myPageScreen.jsx
+++ src/components/members/myPageScreen.jsx
@@ -16,8 +16,30 @@ const MyPageScreen = () => {
     <div className="myPageContainer">
       <MainHeader />
       <div className="myPageBodyArea">
-        <div className="myPageBoxArea"></div>
-        <PrimaryButton buttonText={"수정하기"} onClick={() => {}} />
+        <div className="myPageBoxArea">
+          <div className="myPageUserEmailArea">
+            <div className="myUserInfoLabel">아이디</div>
+            <div className="myUserInfoContent">abc@test.com</div>
+          </div>
+          <div className="myPageUserNameArea">
+            <div className="myUserInfoLabel">이름</div>
+            <div className="myUserInfoContent">김뷰엘</div>
+          </div>
+          <div className="myPageUserPwArea">
+            <div className="myUserInfoLabel">비밀번호 변경</div>
+            <InputBar
+              placeHolder={"새 비밀번호"}
+              isPassword={true}
+              onChange={() => {}}
+            />
+            <InputBar
+              placeHolder={"비밀번호 확인"}
+              isPassword={true}
+              onChange={() => {}}
+            />
+          </div>
+        </div>
+        <PrimaryButton buttonText={"변경하기"} onClick={() => {}} />
         <PrimaryButton
           buttonText={"돌아가기"}
           onClick={() => navigate("/bank")}
