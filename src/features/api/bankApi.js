4b7e3f1030fd5eb2cc4ba7d2300d1d506451a560
Gary Lee <dokdo2005@gmail.com>
2022-01-17 21:13:25 +0900

    axios 에러 핸들링 추가

--- src/features/api/bankApi.js
+++ src/features/api/bankApi.js
@@ -1,21 +1,44 @@
-import { statsUri, thisYearBankListUri, deleteBankUri } from './apiRouteList';
+import { statsUri, thisYearBankListUri, bankDetailUri, deleteBankUri } from './apiRouteList';
 const axios = require('axios').default;
 
 export const bankTotalStatsApi = async () => {
-    const result = await axios.get(statsUri);
-    return result.data;
+    try {
+        const result = await axios.get(statsUri);
+        return result.data;
+    } catch (err) {
+        return err.response.data;
+    }
 }
 
 export const getThisYearBankListApi = async ({ token, currentPage }) => {
-    const result = await axios.get(`${thisYearBankListUri}?currentPage=${currentPage}`, {
-        headers: { token }
-    });
-    return result.data;
+    try {
+        const result = await axios.get(`${thisYearBankListUri}?currentPage=${currentPage}`, {
+            headers: { token }
+        });
+        return result.data;
+    } catch (err) {
+        return err.response.data;
+    }
+}
+
+export const getBankDetailApi = async ({ token, bankId }) => {
+    try {
+        const result = await axios.get(`${bankDetailUri}/${bankId}`, {
+            headers: { token }
+        });
+        return result.data;
+    } catch (err) {
+        return err.response.data;
+    }
 }
 
 export const deleteBankEntryApi = async ({ token, bankId }) => {
-    const result = await axios.delete(`${deleteBankUri}/${bankId}`, {
-        headers: { token }
-    });
-    return result.data;
+    try {
+        const result = await axios.delete(`${deleteBankUri}/${bankId}`, {
+            headers: { token }
+        });
+        return result.data;
+    } catch (err) {
+        return err.response.data;
+    }
 }
