c749f18249cdd78fd0656efa06c4f8821c0c3f4b
Gary Lee <dokdo2005@gmail.com>
2022-01-17 21:23:24 +0900

    axios 에러 핸들링 추가

--- src/features/api/userApi.js
+++ src/features/api/userApi.js
@@ -2,18 +2,30 @@ import { loginUri, joinUri, logoutUri } from './apiRouteList';
 const axios = require('axios').default;
 
 export const userLoginApi = async ({ userEmail, userPw }) => {
-    const result = await axios.post(loginUri, { userEmail, userPw });
-    return result.data;
+    try {
+        const result = await axios.post(loginUri, { userEmail, userPw });
+        return result.data;
+    } catch (err) {
+        return err.response.data;
+    }
 }
 
 export const userJoinApi = async ({ userEmail, userPw, userName }) => {
-    const result = await axios.post(joinUri, { userEmail, userPw, userName });
-    return result.data;
+    try {
+        const result = await axios.post(joinUri, { userEmail, userPw, userName });
+        return result.data;
+    } catch (err) {
+        return err.response.data;
+    }
 }
 
 export const userLogoutApi = async ({ token }) => {
-    const result = await axios.post(logoutUri, null, {
-        headers: { token }
-    });
-    return result.data;
+    try {
+        const result = await axios.post(logoutUri, null, {
+            headers: { token }
+        });
+        return result.data;
+    } catch (err) {
+        return err.response.data;
+    }
 }
